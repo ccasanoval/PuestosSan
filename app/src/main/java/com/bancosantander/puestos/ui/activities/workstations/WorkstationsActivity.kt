ab82d5245b386ddd546a6be58dfba119ef7f4eb9
Borja Angulo <bangulo@serbatic.es>
2017-12-18 16:39:59 +0100

    TabsActivity

--- app/src/main/java/com/bancosantander/puestos/ui/activities/workstations/WorkstationsActivity.kt
+++ app/src/main/java/com/bancosantander/puestos/ui/activities/workstations/WorkstationsActivity.kt
@@ -2,7 +2,13 @@ package com.bancosantander.puestos.ui.activities.workstations
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
+import android.support.v7.widget.Toolbar
+import android.view.View
 import com.bancosantander.puestos.R
+import com.bancosantander.puestos.ui.adapters.WorkstationsPageAdapter
 import com.bancosantander.puestos.ui.presenters.WorkstationsPresenter
 import com.bancosantander.puestos.ui.views.MainViewContract
 import com.mibaldi.viewmodelexamplemvp.base.BaseMvpActivity
@@ -14,17 +20,34 @@ class WorkstationsActivity : BaseMvpActivity<MainViewContract.View,
         WorkstationsPresenter>(),
         MainViewContract.View {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.workstations_activity)
-        mPresenter.init()
-    }
-
-    override var mPresenter: WorkstationsPresenter  = WorkstationsPresenter()
+    private var mWorkstationsPagerAdapter: WorkstationsPageAdapter? = null
+    private var mViewPager: ViewPager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.workstations_activity)
+        mPresenter.init()
+
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val tabsList = arrayListOf(Fragment(),Fragment())
+
+        mWorkstationsPagerAdapter = WorkstationsPageAdapter( supportFragmentManager,
+                                                            this,
+                                                                tabsList)
+
+        mViewPager = findViewById(R.id.workstations_container)
+        mViewPager!!.adapter = mWorkstationsPagerAdapter
+
+        val tabLayout = findViewById<View>(R.id.tabs) as TabLayout
+        tabLayout.setupWithViewPager(mViewPager)
+
+        tabLayout.getTabAt(0)!!.setText("Lista de puestos")
+        tabLayout.getTabAt(1)!!.setText("Mapa de puestos")
     }
+
+    override var mPresenter: WorkstationsPresenter  = WorkstationsPresenter()
 
     override fun onDestroy() {
         super.onDestroy()
