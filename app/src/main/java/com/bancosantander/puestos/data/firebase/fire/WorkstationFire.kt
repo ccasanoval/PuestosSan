0fde9fd07e4046559866eafff4cf54aebadd0b56
Mikel Balduciel Díaz <apps.apple.santander@gmail.com>
2018-01-03 15:29:27 +0100

    release and fill with dates

--- app/src/main/java/com/bancosantander/puestos/data/firebase/fire/WorkstationFire.kt
+++ app/src/main/java/com/bancosantander/puestos/data/firebase/fire/WorkstationFire.kt
@@ -1,17 +1,24 @@
 package com.bancosantander.puestos.data.firebase.fire
 
+import android.support.annotation.WorkerThread
 import android.support.v7.app.AppCompatActivity
 import com.bancosantander.puestos.data.models.User
 import com.bancosantander.puestos.data.models.Workstation
 import com.bancosantander.puestos.util.Log
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.FirebaseFirestoreException
-import com.google.firebase.firestore.GeoPoint
-import com.google.firebase.firestore.QuerySnapshot
+import java.util.*
+import kotlin.collections.HashMap
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.firestore.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
 
 object WorkstationFire {
 	private val TAG: String = WorkstationFire::class.java.simpleName
 	private val ROOT_COLLECTION = "workstations"
+	private val ROOT_COLLECTION_DATES_FREE = "datesFree"
+	private val ROOT_COLLECTION_DATES_OCCUPIED = "datesOccupied"
+	private val ROOT_SUBCOLLECTION = "puestos"
 	private val POSITION_FIELD = "position"
 
 	fun getAll(fire: Fire, callback: (ArrayList<Workstation>, Throwable?) -> Unit) {
@@ -50,7 +57,36 @@ object WorkstationFire {
 					}
 				})
 		}
+	fun getFreeWithDateRT(fire: Fire, date: String, callback: (ArrayList<Workstation>, Throwable?) -> Unit) {
+		val document = fire.getCol(ROOT_COLLECTION_DATES_FREE).document(date)
+		document.collection(ROOT_SUBCOLLECTION)
+				.addSnapshotListener({ data: QuerySnapshot?, error: FirebaseFirestoreException? ->
+                    val res = arrayListOf<Workstation>()
+                    if(error == null && data != null) {
+                        if (data.isEmpty){
+                            callback(res,null)
+                        }
+                        data.forEach { doc ->
+                            val owner = doc.data["owner"] as String
+                            getWorkstation(fire,owner, { workstation, throwable ->
+                                if(throwable == null) {
+                                    res.add(workstation)
+                                    callback(res, null)
+                                }
+                                else {
+                                    callback(res, null)
+                                    Log.e(TAG, "getWorkstation:e:----------------------------------------------------", throwable)
+                                }
+                            })
+                        }
+                    }
+                    else {
+                        callback(res, error)
+                        Log.e(TAG, "getAllRT:e:----------------------------------------------------", error)
+                    }
 
+				})
+	}
 	fun createPuestoHelper(fire: Fire, doc: DocumentSnapshot): Workstation? {
 		val puesto = fire.translate(doc, Workstation::class.java) as Workstation?
 		if(puesto != null) {
@@ -96,7 +132,6 @@ object WorkstationFire {
 						Log.e(TAG, "getAllRT:e:----------------------------------------------------", error)
 					}
 				})
-
 	}
     fun releaseMyWorkstation(fire:Fire,owner: String ,callback: (Workstation, Throwable?) -> Unit){
         fire.getCol(ROOT_COLLECTION)
@@ -119,10 +154,28 @@ object WorkstationFire {
                         Log.e(TAG, "getAll:e:------------------------------------------------------", task.exception)
                     }
                 })
+    }
 
+    fun releaseMyWorkstationV2(fire:Fire,owner: String,date: String,callback: (Workstation, Throwable?) -> Unit){
+
+        val workstationsFree = fire.getCol(ROOT_COLLECTION_DATES_OCCUPIED).document(date).collection(ROOT_SUBCOLLECTION)
+        val queryFree = workstationsFree.whereEqualTo("owner", owner)
+        doAsync {
+            deleteQueryBatch(queryFree)
+        }
+
+        doAsync {
+            val workstationsOccupied = fire.getCol(ROOT_COLLECTION_DATES_FREE).document(date).collection(ROOT_SUBCOLLECTION)
+            val hashMap = HashMap<String, Any>()
+            hashMap.put("owner",owner)
+            workstationsOccupied.add(hashMap)
+            uiThread {
+                releaseMyWorkstation(fire,owner,callback)
+            }
+        }
     }
 	fun fillWorkstation (fire:Fire,owner: String,user : String ,callback: (Workstation, Throwable?) -> Unit) {
-		fire.getCol(ROOT_COLLECTION)
+        fire.getCol(ROOT_COLLECTION)
 				.whereEqualTo("idOwner", owner)
 				.get()
 				.addOnCompleteListener({task ->
@@ -142,4 +195,42 @@ object WorkstationFire {
 					}
 				})
 	}
+    fun fillWorkstationV2 (fire:Fire,owner: String,user : String ,date:String,callback: (Workstation, Throwable?) -> Unit) {
+        val workstationsFree = fire.getCol(ROOT_COLLECTION_DATES_FREE).document(date).collection(ROOT_SUBCOLLECTION)
+        val queryFree = workstationsFree.whereEqualTo("owner", owner)
+        doAsync {
+            deleteQueryBatch(queryFree)
+        }
+
+
+        doAsync {
+            val workstationsOccupied = fire.getCol(ROOT_COLLECTION_DATES_OCCUPIED).document(date).collection(ROOT_SUBCOLLECTION)
+            val hashMap = HashMap<String, Any>()
+            hashMap.put("owner",owner)
+            hashMap.put("idUser",user)
+            workstationsOccupied.add(hashMap)
+            uiThread {
+               fillWorkstation(fire,owner,user,callback)
+            }
+        }
+    }
+
+    /**
+     * Delete all results from a query in a single WriteBatch. Must be run on a worker thread
+     * to avoid blocking/crashing the main thread.
+     */
+    @WorkerThread
+    @Throws(Exception::class)
+    private fun deleteQueryBatch(query: Query): List<DocumentSnapshot> {
+
+        val querySnapshot = Tasks.await(query.get())
+
+        val batch = query.getFirestore().batch()
+        for (snapshot in querySnapshot) {
+            batch.delete(snapshot.getReference())
+        }
+        Tasks.await<Void>(batch.commit())
+
+        return querySnapshot.getDocuments()
+    }
 }
