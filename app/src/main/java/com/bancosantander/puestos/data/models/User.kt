422eea7d396cb7ce456cdc17112e3b9a0e1e50de
Borja Angulo <bangulo@serbatic.es>
2018-01-15 13:19:41 +0100

    User class

--- app/src/main/java/com/bancosantander/puestos/data/models/User.kt
+++ app/src/main/java/com/bancosantander/puestos/data/models/User.kt
@@ -4,6 +4,7 @@ package com.bancosantander.puestos.data.models
 data class User(
 	val id: String = "",
 	val name: String = "",
+	val fullname: String = "",
 	val hadChangedPass: Boolean = false,
 	val type: Type = Type.Interim){
 	enum class Type(name: String) { Admin("Admin"), Fixed("Fixed"), Interim("Interim") }
